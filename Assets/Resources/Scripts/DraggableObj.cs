660836452a64f7fa43c5f3fc001751d8179eec46
zappknight <victorfernandes.nave@gmail.com>
2015-07-10 08:26:07 -0300

    Gerenc e Camera Follow
    Adicionamos um script que seguirá o player e um gerenciador de jogo.

---
+++ Assets/Resources/Scripts/DraggableObj.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggableObj : Default {
+    public string match;
+    public bool canDrag;
+	// Use this for initialization
+	void Start () {
+        canDrag = true;
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Debug.Log("ooooooooooi");
+        if (col.gameObject.tag.Equals(match) )
+        {
+            transform.position = col.gameObject.transform.position;
+            Debug.Log("ooooooooooi");
+            Destroy(col.gameObject);
+            canDrag = false;
+        }
+        
+    }
+
+    public override void OnTouch(Touch t)
+    {
+        if (canDrag)
+        {
+            Vector3 r = Camera.main.ScreenToWorldPoint(t.position);
+            r = new Vector3(r.x, r.y, 0);
+            transform.position = r;
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        base.Update();
+	}
+}
---
+++ Assets/Resources/Scripts/Gerenc.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gerenc : Default {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void Puzzle1()
+    {
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;                                                                 
+	}
+}
