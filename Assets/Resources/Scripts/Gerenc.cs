05f49e635d4de3215a1bf06f8a56b80be3a9a145
zappknight <victorfernandes.nave@gmail.com>
2015-07-10 08:33:31 -0300

    Desenvolvimento dos Puzzles 3

--- Assets/Resources/Scripts/Gerenc.cs
+++ Assets/Resources/Scripts/Gerenc.cs
@@ -2,75 +2,22 @@
 using System.Collections;
 
 public class Gerenc : Default {
-    GameObject player;
-    public static bool outOfBus = false;
-    GameObject savedMap;
+    public GameObject player;
+    Player pScript;
+    public bool bus;
     public static bool goodAction = false;
 
-    IEnumerator StartBusTimer()
-    {
-        yield return new WaitForSeconds(6f);
-        if (GameObject.FindGameObjectWithTag("Seat").GetComponent<DraggableObj>().matchFound)
-        {
-            EndBusPuzzle();
-        }
-        else Debug.Log("Perdeu");
-    }
-
 	// Use this for initialization
-	void Start () {
-        if(Application.loadedLevelName.Equals("inBus"))
-        {
-            Debug.Log("START");
-            savedMap = GameObject.FindGameObjectWithTag("DontDestroy");
-            if (savedMap)
-            {
-                Debug.Log("TESTE");
-                savedMap.SetActive(false);
-            }
-            StartCoroutine(StartTimerForBus());
-        }
-        if (Application.loadedLevelName.Equals("inGame"))
-        {
-            if(!Gerenc.outOfBus && !GameObject.FindGameObjectWithTag("DontDestroy"))
-            {
-                Debug.Log("ooi");
-                GameObject map = Resources.Load("Prefabs/InGameMap") as GameObject;
-                Instantiate(map);
-            }
-            else if (Gerenc.outOfBus && GameObject.FindGameObjectWithTag("DontDestroy"))
-            {
-                Destroy(GameObject.FindGameObjectsWithTag("DontDestroy")[1]);
-                GameObject wayP = GameObject.Find("WayPoint1");
-                player = GameObject.FindGameObjectWithTag("Player");
-                player.transform.position = new Vector2(wayP.transform.position.x - 0.9f,wayP.transform.position.y);
-                player.GetComponent<Player>().current = GameObject.Find("WayPoint1");
-            }
-        }
-        player = GameObject.FindGameObjectWithTag("Player");
+	void Start () 
+    {
+        pScript = player.GetComponent<Player>();
+        
     }
 
-    IEnumerator StartTimerForBus()
+    IEnumerator StartBusWalking()
     {
-        yield return new WaitForSeconds(4f);
-        if (GameObject.FindObjectOfType<DraggableObj>().matchFound) EndBusPuzzle();
-    }
-
-    void StartBusPuzzle()
-    {
-        if(Application.loadedLevelName.Equals("inGame"))
-        {
-            Object.DontDestroyOnLoad( GameObject.FindGameObjectWithTag("DontDestroy"));
-            Application.LoadLevel("inBus");
-        }
-    }
-
-    void EndBusPuzzle()
-    {
-        DontDestroyOnLoad(savedMap);
-        savedMap.SetActive(true);
-        Gerenc.outOfBus = true;
-        Application.LoadLevel("inGame");
+        yield return new WaitForSeconds(2f);
+        pScript.isWalking = true;
     }
 
 	void CorrectChoice()
@@ -80,12 +27,17 @@ public class Gerenc : Default {
 
 	void WrongChoice()
 	{
-        StartBusPuzzle();
+        pScript.isWalking = false;
+        Fade.fadeIn();
+        bus = false;
 	}
 
 	// Update is called once per frame
     public override void Update()
     {
+        if( !bus && Fade.alpha == 1 )
+            //player.GetComponent<SpriteRenderer>().sprite = Resource
+
         Screen.orientation = ScreenOrientation.LandscapeLeft;                                                                 
 	}
 }
